f788d669ea122065eaeab45732a3af1aa8407c6a
Max-Tsymba <young.bezero@gmail.com>
2021-07-18 16:56:52 +0300

    done header + main

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,6 @@ import mobileMenuOpener from './modules/menu';
 import rotateText from './modules/rotate';
 
 window.addEventListener('DOMContentLoaded', () => {
-    // mobileMenuOpener('burger', '.menu__list', '.menu__list-link');
+    mobileMenuOpener('burger', '.menu__wrapper', '.menu__list-link');
     rotateText('.text-rotate');
 });
