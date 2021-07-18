7bbd6dd818258169cf62d227083aaf6f2ac1d0fe
Max-Tsymba <young.bezero@gmail.com>
2021-07-19 00:55:08 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,57 @@ window.addEventListener('DOMContentLoaded', () => {
     mobileMenuOpener('burger', '.menu__wrapper', '.menu__list-link');
     rotateText('.text-rotate');
     resizeTextArea();
+
+    let mySlider = new Swiper('.swiper-container', {
+
+        navigation: {
+            nextEl: '.next-btn',
+            prevEl: '.prev-btn',
+        },
+
+
+        simulateTouch: false,
+        autoHeight: true,
+        slidesPerView: 3,
+        slidesPerGroup: 1,
+        loop: true,
+
+        autoplay: {
+            delay: 2000,
+            stopOnLastSlide: false,
+            disableOnInteraction: false,
+        },
+
+        speed: 1000,
+
+        breakpoints: {
+            1501: {
+                spaceBetween: 12,
+            },
+            1025: {
+                slidesPerView: 3,
+            },
+            769: {
+                slidesPerView: 2,
+            },
+            279: {
+                slidesPerView: 1,
+            },
+            0: {
+                slidesPerView: 1,
+            }
+        }
+    });
+
+    let sliderBack = document.querySelector('.portfolio__container');
+
+    sliderBack.addEventListener('mouseleave', (e) => {
+        mySlider.params.autoplay.disableOnInteraction = false;
+        mySlider.params.autoplay.delay = 1000;
+        mySlider.autoplay.start();
+    });
+
+    sliderBack.addEventListener('mouseenter', (e) => {
+        mySlider.autoplay.stop();
+    })
 });
