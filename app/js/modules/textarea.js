3b30319c39071c4b842578f84736cb970518b4a4
Max-Tsymba <young.bezero@gmail.com>
2021-07-21 19:07:30 +0300

    fix

--- app/js/modules/textarea.js
+++ app/js/modules/textarea.js
@@ -1,12 +1,23 @@
 const resizeTextArea = () => {
     
     let textareas = document.querySelectorAll('textarea');
+    let group = document.querySelector('.text-group');
     for (var i = 0; i < textareas.length; i++) {
       textareas[i].addEventListener("input", function(e){
         this.style.height = "auto";
         this.style.height = this.scrollHeight + "px";      
       });
+      if(textareas[i].value === '') group.classList.remove('down');
     }
+
+
+    textareas[0].addEventListener('keyup', ()=>{
+      if(textareas[0].value === ''){
+        group.classList.remove('down');
+      } else {
+        group.classList.add('down');
+      }
+    });
 };
 
 export default resizeTextArea;
